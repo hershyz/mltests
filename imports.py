52f3d9cf7976f8eebae33516220338c8d6045cf7
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-05-19 14:15:36 -0400

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
+import numpy as np                                      # optimized array library for faster operations
+import pandas as pd                                     # data manipulation library
+import matplotlib.pyplot as plt                         # visualization library (graphs and charts)
+from IPython.display import clear_output                
 from six.moves import urllib
 
-import tensorflow.compat.v2.feature_column as fc
+import tensorflow.compat.v2.feature_column as fc        # feature column (used for linear regression algorithm)
 
-import tensorflow as tf
+import tensorflow as tf                                 # tensorflow
