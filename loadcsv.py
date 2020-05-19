97ce1d5b2d81fbe7f21ca5f4946ac4ed2e9b41c8
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-05-19 14:23:43 -0400

    Create loadcsv.py

---
+++ loadcsv.py
@@ -0,0 +1,4 @@
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
